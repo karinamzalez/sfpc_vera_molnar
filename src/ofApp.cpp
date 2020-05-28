a70f7d79371ac4c9a459ea3477cc4de0abf166cc
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2020-05-28 01:22:47 -0500

    add slight randomness to line start/end -- start and end randomness are interdependent to create subtle wave effect

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,10 +3,16 @@
 //Question: Where would be a good place to store global variables?
 int windowWidth = 800;
 int windowHeight = 800;
+vector<int> randomStartArr;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetWindowShape(windowWidth, windowHeight);
     ofSetBackgroundColor(238, 234, 233);
+    for (int i = 0; i < 20; i++) {
+//        QUESTION: what's the difference b/w ofRandom and ofNoise
+        randomStartArr.push_back(ofRandom(-50, 50));
+    }
 }
 
 //--------------------------------------------------------------
@@ -17,6 +23,9 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     // Draw square of horizontal lines:
+    // NOTE:
+//    * her lines aren't uniform length
+//    * starting & ending points vary but when start is to the left, so is the end -- makes a subtle wave effect
     // QUESTION: would it be possible to utilize ofDrawGrid to draw horizontal lines?
     ofSetColor(0); // set color of lines to black
     // QUESTION: Is there a way to dynamically get window WxH to dynamically set margin?
@@ -25,10 +34,10 @@ void ofApp::draw(){
 //      QUESTION: Is there a way to see the coordinate grid before drawing lines?
         int newYCoordinate = margin + (((windowHeight - (margin*2)) / 20) * i); // evenly distribute lines (vertically) within margin
         ofDrawLine(
-                   margin,
+                   margin + randomStartArr[i],
                    newYCoordinate,
-                   windowHeight - margin, // set uniform line length
-                   newYCoordinate);
+                   windowWidth - (margin - randomStartArr[i]), // note: ofRandomWidth() animates random widths
+                   newYCoordinate); // y coordinate remains the same bc line is straight (ofRandomHeight is fun!)
     }
 }
 
