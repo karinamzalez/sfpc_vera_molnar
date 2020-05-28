10ff43727a11b6af4e08a1ff6d5f06ad4a54fed3
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2020-05-28 12:03:58 -0500

    toggle randomness

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,11 +13,20 @@ vector<int> randomSlopeArr2;
 vector<int> ramdomInterruptionStartArr;
 vector<int> ramdomInterruptionHeightArr;
 
+bool toggleHorMove=false;
+bool toggleHorMove2=false;
+bool toggleSlantMove=false;
+bool toggleSlantMove2=false;
+bool invertColor=false;
+bool randomColor=false;
+bool randomDensity=false;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetWindowShape(windowWidth, windowHeight);
     ofSetBackgroundColor(238, 234, 233);
     for (int i = 0; i < 20; i++) {
+//        TODO: move random generator to resusable function
 //        QUESTION: what's the difference b/w ofRandom and ofNoise
         randomStartArr.push_back(ofRandom(-50, 50));
         randomLengthArr.push_back(ofRandom(-20, 20));
@@ -31,8 +40,8 @@ void ofApp::setup(){
     for (int j=0; j < 40; j++) {
         randomSlopeArr.push_back(ofRandom(10, 40));
         randomSlopeArr2.push_back(ofRandom(20, 150));
-        ramdomInterruptionStartArr.push_back(ofRandom(100, 700));
-        ramdomInterruptionHeightArr.push_back(ofRandom(100, 600));
+        ramdomInterruptionStartArr.push_back(ofRandom(90, 620));
+        ramdomInterruptionHeightArr.push_back(ofRandom(100, 635));
     }
     
     
@@ -49,14 +58,57 @@ void ofApp::draw(){
     // NOTE:
 //    * her lines aren't uniform width
 //    * starting & ending points vary but when start is to the left, so is the end -- makes a subtle wave effect
+    float time = ofGetElapsedTimef();
     
+    if (randomDensity) {
+        randomStartArr = {};
+        randomLengthArr = {};
+        randomGapArr = {};
+        randomGap = {};
+        randomGapLength = {};
+        randomSlopeArr = {};
+        randomSlopeArr2 = {};
+        ramdomInterruptionStartArr = {};
+        ramdomInterruptionHeightArr ={};
+        
+                for (int i = 0; i < 20; i++) {
+            //        TODO: move random generator to resusable function
+            //        QUESTION: what's the difference b/w ofRandom and ofNoise
+                    randomStartArr.push_back(ofRandom(-50, 50));
+                    randomLengthArr.push_back(ofRandom(-20, 20));
+            //        QUESTION: How to get random integer value instead of percentage?
+                    randomGapArr.push_back(ofRandom(1, 10));
+                    randomGap.push_back(ofRandom(2, 15));
+                    randomGapLength.push_back(ofRandom(10, 600));
+                }
+                
+            //    40 because there are more slanted lines so we need more variations
+                for (int j=0; j < 40; j++) {
+                    randomSlopeArr.push_back(ofRandom(10, 40));
+                    randomSlopeArr2.push_back(ofRandom(20, 150));
+                    ramdomInterruptionStartArr.push_back(ofRandom(90, 620));
+                    ramdomInterruptionHeightArr.push_back(ofRandom(100, 635));
+                }
+    }
+    
+    
+    
+    
+    
+    if (randomColor) {
+            ofSetBackgroundColor(ofRandom(255),
+                                 ofRandom(255),
+                                 ofRandom(255));
+    } else {
+        invertColor ? ofSetBackgroundColor(0) : ofSetBackgroundColor(238, 234, 233);
+    }
     // QUESTION: would it be possible to utilize ofDrawGrid to draw horizontal lines?
-    ofSetColor(0x000000, 128); // set color of lines to slightly opaque black to emulate pen tracing over
+    invertColor ? ofSetColor(238, 234, 233) : ofSetColor(0x000000, 128); // set color of lines to slightly opaque black to emulate pen tracing over
     // QUESTION: Is there a way to dynamically get window WxH to dynamically set margin?
     int margin = 100;
     for (int i=0; i < 20; i++) {
 //      QUESTION: Is there a way to see the coordinate grid before drawing lines?
-        int lineWidth = (windowWidth - randomLengthArr[i]); // add slight variation to width
+        int lineWidth = (windowWidth - (randomDensity ? ofRandom(2, 100) : randomLengthArr[i])); // add slight variation to width
         int lineEnd = lineWidth - (margin - randomStartArr[i]); // make line ending point correlated to starting
         int newYCoordinate = margin + (((windowHeight - (margin*2)) / 20) * i); // evenly distribute lines (vertically) within margin
         for (int j=0; j < 6; j++) {
@@ -74,53 +126,72 @@ void ofApp::draw(){
                 ofDrawLine(
                            margin + randomStartArr[i] + j,
                            newYCoordinate,
-                           firstLineEnd - j, // note: ofRandomWidth() animates random widths
+                           toggleHorMove ? ofRandomWidth() : firstLineEnd - j, // note: ofRandomWidth() animates random widths
                            newYCoordinate); // y coordinate remains the same bc line is straight (ofRandomHeight is fun!)
 
                 ofDrawLine(
                            firstLineEnd + (randomGap[i]), // variable gap length
                            newYCoordinate,
-                           lineEnd + (j *6), // note: ofRandomWidth() animates random widths
+                           toggleHorMove ? ofRandomWidth() : lineEnd + (j *6), // note: ofRandomWidth() animates random widths
                            newYCoordinate); // y coordinate remains the same bc line is straight (ofRandomHeight is fun!)
             } else {
                 ofDrawLine(
                           (margin + randomStartArr[i]),
                           newYCoordinate,
-                          lineEnd - (j * 6), // note: ofRandomWidth() animates random widths
+                          toggleHorMove2 ? ofRandomWidth() : lineEnd - (j * 6), // note: ofRandomWidth() animates random widths
                           newYCoordinate); // y coordinate remains the same bc line is straight (ofRandomHeight is fun!)
             }
         }
         
-        
+//        TODO: maybe try intercepting one or two slanted lines with the gap
+//        TODO: get subtle slope when drawing lines
 //        SLANTED line interruptions:
 //        NOTE: all of her slopes are positive and over 90 degrees.
-        ofSetColor(0x000000); // set color of lines to slightly opaque black to emulate pen tracing over
+        invertColor ? ofSetColor(238, 234, 233) : ofSetColor(0); // set color of lines to slightly opaque black to emulate pen tracing over
         ofSetLineWidth(2); // slightly vary line width to emulate hand writing
 
         for (int k = 0; k < 40; k ++) {
-            float centerX = ramdomInterruptionStartArr[k];
-            float centerY = ramdomInterruptionHeightArr[k];
+            float centerX = toggleSlantMove ? ofRandom(90, 620) : ramdomInterruptionStartArr[k];
+            float centerY = toggleSlantMove2 ? ofRandom(100, 635) : ramdomInterruptionHeightArr[k];
           
             float slopeX = randomSlopeArr2[k];
-            float slopeY = randomSlopeArr[k] + 10;
+            float slopeY = randomSlopeArr[k] + k * 2;
 
-//            RISE OVER RUN :) 
+//            RISE OVER RUN :)
             ofDrawLine(centerX + slopeX,
                        centerY - slopeY,
                        centerX,
                        centerY
                        );
         };
+        
+        
+        
+        
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key) {
+    if (key == 'z') {
+        toggleHorMove = !toggleHorMove;
+    } else if (key == 'x') {
+        toggleHorMove2 = !toggleHorMove2;
+    } else if (key == 'c') {
+        toggleSlantMove = !toggleSlantMove;
+    } else if (key == 'v') {
+        toggleSlantMove2 = !toggleSlantMove2;
+    } else if (key == 'b') {
+        invertColor = !invertColor;
+    } else if (key == 'n') {
+        randomColor = !randomColor;
+    } else if (key == 'm') {
+        randomDensity = !randomDensity;
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
